18957501317b8564e589d65073d0c976b233ec1c
Allan Calix <contact@allancalix.com>
2020-03-01 21:57:06 -0800

    Score the final resulting sets and print the lowest score

--- 1-6-cipher/src/main.rs
+++ 1-6-cipher/src/main.rs
@@ -20,27 +20,27 @@ fn guess_key_size(cipher: &Vec<u8>) -> Vec<u8> {
             .skip(key_size * 2)
             .take(key_size)
             .collect();
-        let forth_chunk: Vec<u8> = cipher
+        let fourth_chunk: Vec<u8> = cipher
             .clone()
             .into_iter()
             .skip(key_size * 3)
             .take(key_size)
             .collect();
 
-        let dist1 = xor::hamming_distance(first_chunk, second_chunk) / key_size as u32;
-        let dist2 = xor::hamming_distance(third_chunk, forth_chunk) / key_size as u32;
-        let mean = vec!(dist1, dist2).into_iter().map(f64::from).sum::<f64>() / f64::from(2);
+        let dist1 = xor::hamming_distance(first_chunk.clone(), second_chunk) / key_size as u32;
+        let dist2 = xor::hamming_distance(first_chunk.clone(), third_chunk) / key_size as u32;
+        let dist3 = xor::hamming_distance(first_chunk.clone(), fourth_chunk) / key_size as u32;
+        let mean = vec!(dist1, dist2, dist3).into_iter().map(f64::from).sum::<f64>() / f64::from(3);
         result.push((mean, key_size as u8));
     }
     result.sort_by(|x, y| x.0.partial_cmp(&y.0).unwrap());
     // Return top three results.
-    result.iter().map(|x| x.1).skip(6).take(1).collect::<Vec<u8>>()
+    result.iter().map(|x| x.1).take(4).collect::<Vec<u8>>()
 }
 
 fn main() {
     let p: Vec<String> = env::args().collect();
     let cipher_file = Path::new(p.get(1).unwrap().as_str());
-    let size = p.get(2).unwrap().parse::<i32>().unwrap();
     let mut f = File::open(cipher_file).unwrap();
 
     let mut cipher = String::new();
@@ -48,18 +48,23 @@ fn main() {
 
     // Flatten the base64 cipher into a continuous string before decoding.
     let buf = base64::decode(cipher.replace("\n", "").as_str()).unwrap();
-    let sizes = vec!(size);
-    // let sizes = guess_key_size(&buf);
+    let sizes = guess_key_size(&buf);
 
     let keys: Vec<Vec<char>> = sizes
         .into_iter()
         .map(|size| xor::do_key_speculation(&buf, size as usize))
         .collect();
 
+    let mut results = vec!();
     for key in keys {
         let key: String = key.iter().collect();
         // Decipher original text.
         let decrypted = xor::decrypt(&buf, key.as_str());
-        println!("{}", String::from_utf8(decrypted).unwrap());
+        results.push((
+            xor::score(String::from_utf8(decrypted.clone()).unwrap().as_str()),
+            decrypted,
+        ));
     }
+    results.sort_by(|x, y| x.0.cmp(&y.0));
+    println!("{}", String::from_utf8(results.first().unwrap().1.clone()).unwrap());
 }
